c55285f0612a99814b93597ad3a64ab6097a555d
willian.acarvalho <willian.acarvalho@CAS555069W0701.senacsp.edu.br>
2016-08-19 20:32:40 -0300

    terceiro commit

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -2,6 +2,6 @@ package br.senac.tads3.agenda;
 
 public class Agenda {
     public static void main(String[] args) {
-        System.out.println("xxxxxxxx");
+        System.out.println(":((((((((((((");
     }
 }
